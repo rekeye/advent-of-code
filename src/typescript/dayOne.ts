a39670d0a51e0132eb4c1b03485af595ab60305d
rekeye <12szymon.paluch34@gmail.com>
2021-12-03 23:16:39 +0100

    Resolve part two of day one

--- src/typescript/dayOne.ts
+++ src/typescript/dayOne.ts
@@ -4,6 +4,15 @@ import { countIncreased } from '../helpers/countIncreased';
 const inputs = getInputs('./_inputs/dayOne.txt');
 console.log('array of inputs: ', inputs);
 
-// part one
+// PART ONE
 const partOne = countIncreased(inputs);
 console.log('number of increases: ', partOne);
+
+// PART TWO
+const arrayOfThrees = inputs
+  .filter((value, index, array) => index < array.length - 2)
+  .map((current, index) => current + inputs[index + 1] + inputs[index + 2]);
+console.log('array of threes: ', arrayOfThrees);
+
+const partTwo = countIncreased(arrayOfThrees);
+console.log('number of increases based on three measurement windows: ', partTwo);
